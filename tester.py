9cb7c73739d1a75a8b8f92a64ead51c3bd8c88e1
KaalkidanSahele <92366908+KaalkidanSahele@users.noreply.github.com>
2023-11-04 12:45:26 +0000

    Delete tester.py

--- tester.py
+++
@@ -1,8 +0,0 @@
-## this is the UDP that lets us connect SonicPi with Python
-
-from pythonosc import osc_message_builder
-from pythonosc import udp_client
-
-sender = udp_client.SimpleUDPClient('127.0.0.1', 36611)
-# Edit params to [70] if just using `play` function on server
-sender.send_message('/bpm', [80, 75, 70, 85] )
